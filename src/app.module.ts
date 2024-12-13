f4e0b570d19d7f6f43db7e4b1ceef30d0b335a69
Muhammad Osama <usamahafiz8@gmail.com>
2024-12-13 16:53:16 +0500

    created the auto swager file system and also first resouse for the User as a Curd Operation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
+  // imports: [
+  //   TypeOrmModule.forRoot({
+  //     type: 'postgres',
+  //     host: 'localhost', // Change this if you're using a different host
+  //     port: 5432,
+  //     username: 'your_db_user', // Replace with your db username
+  //     password: 'your_db_password', // Replace with your db password
+  //     database: 'users_db', // Replace with your database name
+  //     entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  //     synchronize: true, // Set to false in production to avoid data loss
+  //   })],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('AI-Chain ($TAG) Platforn')
+    .setDescription('API Documentation for tag chain')
+    .setVersion('1.0')
+    // .addTag('users') // Add tags for grouping endpoints
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();

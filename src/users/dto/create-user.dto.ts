4a914ffadcd20f5fd94525966920b5df9323a8b2
Muhammad Osama <58689276+Usamahafiz8@users.noreply.github.com>
2024-12-14 21:16:50 +0500

    user and db coinnection

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+import { IsEmail, IsString, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  walletAddress: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsString()
+  nickname: string;
+}
